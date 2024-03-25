887e117eb15140fdbbf2a050e9cc3686dabf8502
airajaneeeee <ajbassig@gmail.com>
2024-03-24 19:07:05 -0700

    new placeholder

--- scripts/main.js
+++ scripts/main.js
@@ -16,6 +16,72 @@ function getNameFromAuth() {
         }
     });
 }
-
-
 getNameFromAuth(); 
+
+
+function writeMenu() {
+    //define a variable for the collection you want to create in Firestore to populate data
+    var menuRef = db.collection("menu");
+
+    console.log("working");
+
+    menuRef.add({
+        code: "MENU01",
+        name: "Tutorials", 
+        last_updated: firebase.firestore.FieldValue.serverTimestamp()  //current system time
+    });
+    menuRef.add({
+        code: "MENU02",
+        name: "Quizzes", //replace with your own city?
+        last_updated: firebase.firestore.FieldValue.serverTimestamp() 
+    });
+    menuRef.add({
+        code: "MENU03",
+        name: "Progress", //replace with your own city?
+        last_updated: firebase.firestore.FieldValue.serverTimestamp() 
+    });
+    menuRef.add({
+        code: "MENU04",
+        name: "News", //replace with your own city?
+        last_updated: firebase.firestore.FieldValue.serverTimestamp() 
+    });
+}
+
+
+
+//------------------------------------------------------------------------------
+// Input parameter is a string representing the collection we are reading from
+//------------------------------------------------------------------------------
+function displayCardsDynamically(collection) {
+    let cardTemplate = document.getElementById("menuCardTemplate"); // Retrieve the HTML element with the ID "hikeCardTemplate" and store it in the cardTemplate variable. 
+
+    db.collection(collection).get()   //the collection called "hikes"
+        .then(allMenu=> {
+            //var i = 1;  //Optional: if you want to have a unique ID for each hike
+            allMenu.forEach(doc => { //iterate thru each doc
+                var title = doc.data().name;       // get value of the "name" key
+                var details = doc.data().details;  // get value of the "details" key
+								var menuCode = doc.data().code;    //get unique ID to each hike to be used for fetching right image
+                var docID = doc.id;
+                let newcard = cardTemplate.content.cloneNode(true); // Clone the HTML template to create a new card (newcard) that will be filled with Firestore data.
+
+                //update title and text and image
+                newcard.querySelector('.card-title').innerHTML = title;
+                newcard.querySelector('.card-text').innerHTML = details;
+                newcard.querySelector('.card-image').src = `./images/${menuCode}.jpg`; //Example: NV01.jpg
+                newcard.querySelector('a').href = "eachMenu.html?docID="+docID;
+                
+                //Optional: give unique ids to all elements for future use
+                // newcard.querySelector('.card-title').setAttribute("id", "ctitle" + i);
+                // newcard.querySelector('.card-text').setAttribute("id", "ctext" + i);
+                // newcard.querySelector('.card-image').setAttribute("id", "cimage" + i);
+
+                //attach to gallery, Example: "hikes-go-here"
+                document.getElementById(collection + "-go-here").appendChild(newcard);
+
+                //i++;   //Optional: iterate variable to serve as unique ID
+            })
+        })
+}
+
+displayCardsDynamically("menu");  //input param is the name of the collection
